24d6a24e4c3e43fa714357d76ae2070488eef36f
Giovanni Chibuzor <giovanni2chibuzor@gmail.com>
2025-04-04 20:14:13 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = input("Name: ")
+age = int(input("age: "))
+height = float(input("height in cm:"))
+
+
+print("Hello, "+name)
+print("your age is: "+str(age))
+print("Your height is: "+str(height))
